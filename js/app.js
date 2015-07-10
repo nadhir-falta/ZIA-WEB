4669212bdf135b42a1d438aba4a08cbacebde42c
nadhir-falta <nxfalta@oaklandcc.edu>
2015-07-10 15:32:42 -0400

    added few other pages

--- js/app.js
+++ js/app.js
@@ -1,3 +1,3 @@
 var app = angular.module("angularHook", []);
 
-app.anglarController("myCtrl");
+app.angularController("myCtrl");
--- js/libs/custom.js
+++ js/libs/custom.js
@@ -7,6 +7,17 @@ jQuery(window).load(function () {
         var height;
         height = jQuery(window).height();
         jQuery('#homepage').css('height', "100vh");
+
+        $('.box').css('display', 'none');
+        for (var i = 0;  i < 7; i++) {
+            $('#my-selector-' + i).css('display', 'none');
+        }
+
+        /******** setting dynamic font sizs to titles ******************************/
+        jQuery(".slogan").fitText(1.2, { minFontSize: '15px', maxFontSize: '60px' }); 
+        jQuery(".site-name").fitText(1.2, { minFontSize: '15px', maxFontSize: '60px' });
+        jQuery(".aboutMeTitle").fitText(1.2, { minFontSize: '15px', maxFontSize: '36px' });
+        jQuery(".weDoBestTitle").fitText(1.2, { minFontSize: '15px', maxFontSize: '34px' });
     }
 
     amend();
@@ -17,24 +28,52 @@ jQuery(window).load(function () {
 
     function scrolling () {
 
-        var ypos = window.pageYOffset + 50;
+        //animation to stick the header on scroll after passing the home page
+        var ypos = window.pageYOffset;
         var $hpHeight = jQuery('#homepage').css("height");
 
         var p = $hpHeight.indexOf('p');
         $hpHeight = parseInt($hpHeight.substring(0, p));
-        if (ypos > ($hpHeight )) {
+        if (ypos > (ypos - 200 )) {
             $('header').removeClass("header");
             $('header').addClass("navbar-fixed-top");
-            $('.box').addClass('animated fadeInDown');
         };
-        if (ypos < $hpHeight) {
+        if (ypos == 0) {
             $('header').removeClass("navbar-fixed-top");
             $('header').addClass("header");
         }
+
+        //animation for about us
+        if (ypos > ($hpHeight - 400 )) {
+            $('.box').css('display', 'block');
+            $('.box').addClass('animated fadeInDown');
+        };
+
+        //animation for What we do best
+        var aboutHeight = jQuery('#tz-about-head').css("height");
+        var px = aboutHeight.indexOf('p');
+        aboutHeight = parseInt(aboutHeight.substring(0, px)) - 200;
+
+        if (ypos > (aboutHeight + $hpHeight )) {
+            
+            for (var i = 0;  i < 7; i++) {
+                $('#my-selector-' + i).css('display', 'inline-block');
+            }
+
+            $('#my-selector-1').addClass('animated fadeInLeft');
+            $('#my-selector-2').addClass('animated fadeInLeft');
+            $('#my-selector-3').addClass('animated fadeInUp');
+            $('#my-selector-4').addClass('animated fadeInUp');
+            $('#my-selector-5').addClass('animated fadeInRight');
+            $('#my-selector-6').addClass('animated fadeInRight');
+            
+        };
     }
     window.addEventListener ("scroll", scrolling);
 });
 
+
+// main background carousel
 jQuery(window).ready(function () {
     jQuery.backstretch([
         "assets/chicago.jpg"
@@ -45,9 +84,7 @@ jQuery(window).ready(function () {
         ], {duration: 5000, fade: 'slow'});
 });
 
-/**********************************/
-/* Run Loading GIF */
-/********************************/
+
 jQuery(window).load(function () {
 
     jQuery("#loading").fadeOut("1500", function () {
@@ -56,80 +93,85 @@ jQuery(window).load(function () {
         jQuery('.loading').css("display", "none");
     });
 
+
+    // function handler for what we do boxs click
+    $("div").on('click', function () {
+        var selector = this.id;
+        if(selector.indexOf("my-selector") >= 0) {
+            for (var i = 0;  i < 7; i++) {
+                $('#formatted-'+i).css('display', 'none');
+                $('#my-selector-' + i).removeClass('active');
+                $('#formatted-'+i).removeClass('active');
+            }
+            setTimeout(function() {
+                var selectorNum = selector.substr(selector.length - 1);
+                $('#formatted-'+selectorNum).css('display', 'block');
+                $('#formatted-'+selectorNum).addClass('active');
+                $('#'+ selector).addClass('active');
+            }, 0);  
+        }
+    });
+
     if (jQuery(window).width() > 767) {
         jQuery('.homepage-inner').addClass('animated fadeInUp');
         jQuery('#button-scroll').addClass('animated bounce');
-        jQuery('.boxes .box').appear(function () {
-            jQuery('.boxes .box').addClass('animated fadeInUp');
-        });
-        jQuery('.offer-box').appear(function () {
-
-            jQuery('.offer-box').addClass('animated fadeInDown');
-
-        });
-        jQuery('.our_team .team_animation').appear(function () {
-
-            jQuery('.our_team .team_animation').addClass('animated fadeInDown');
-
-        });
-
-        jQuery('.customer').appear(function () {
-
-            jQuery('.customer').addClass('animated fadeInDown');
-            jQuery('.customer img').addClass('animated bounceIn');
-            jQuery('.m1').countTo();
-            jQuery('.m2').countTo();
-            jQuery('.m3').countTo();
-            jQuery('.m4').countTo();
-            jQuery('.m5').countTo();
-
-        });
-        jQuery('.purchase  img').appear(function () {
-
-            jQuery('.purchase  img').addClass('animated flipInY');
-
-        });
-        jQuery('#filter').appear(function () {
-
-            jQuery('#filter').addClass('animated slideInDown');
-
-        });
-        jQuery('#portfolio .TzInner').appear(function () {
-
-            jQuery('#portfolio .TzInner').addClass('animated rollIn');
-
-        });
-        jQuery('.blogs ').appear(function () {
-
-            jQuery('img.woman').addClass('animated bounceIn');
-        });
-
-        jQuery('.milestones ').appear(function () {
-            jQuery('img.man').addClass('animated bounceIn')
-        });
-        jQuery('.blog').appear(function () {
-
-            jQuery('.blog').addClass('animated fadeInUp');
-            jQuery('.blog .icon-border span i').addClass('animated bounceIn');
-
-        });
-        jQuery('.logos img').appear(function () {
-
-            jQuery('.logos img').addClass('animated fadeInDown');
-
-        });
+        $('#formatted-1').css('display', 'block');
+        $('#my-selector-1').addClass('active');
+    }
+    if (jQuery(window).width() < 767) {
+        $('.skills').css('background', 'white');
     }
 
+    //flex slider animation
+
+    $('.nav_slide_button').click(function() {
+        $('.pull').slideToggle();
+    });
+
+
+    $('#servicesSlider').flexslider({
+        animation: "slide",
+        touch: true,
+        directionNav: false,
+        pauseOnHover: false,
+        slideshowSpeed: 6000, 
+    });
+});
+
+
+$( window ).resize(function() {
+    var width = jQuery('#homepage').width() - 30;
+
+    if (jQuery(window).width() > 767) {
+        $('#formatted-1').css('display', 'block');
+        $('#my-selector-1').addClass('active');
+
+        $('.skills').css('background', 'url(../assets/pic_skills.jpg) no-repeat center 10px');
+    }
+    else {
+        $('#formatted-1').css('display', 'none');
+        $('#my-selector-1').removeClass('active');
+
+        $('.skills').css('background', 'white');
+    }
+
+        if (jQuery(window).width() > 767) {
+            $('.skills').css('background', 'url(../assets/pic_skills.jpg) no-repeat center 10px');
+        }
+        else {
+            $('.skills').css('background', 'white');
+        }
 });
 
 ///**********************/
 ///* Jquery NAV Plugin */
-///*********************/
+///********************/
+
 jQuery(document).ready(function () {
     "use strict";
     var $nav;
     var $button_scroll;
-    $nav = jQuery('#plazart-mainnav .nav');
+    $nav = jQuery('#nav');
     $button_scroll = jQuery('#button-scroll');
 
     $nav.onePageNav({
@@ -159,9 +201,7 @@ jQuery(document).ready(function () {
         $nav.find('li').eq(currentPos).children('a').trigger('click');
         e.preventDefault();
     });
-
 });
-//
 //
 ////Add fixed position to NAV
 
